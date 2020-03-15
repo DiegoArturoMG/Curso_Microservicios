3e8815b6186bab83f9c1d58d42f07e08b1b46aeb
Diego Arturo MG <diegoarturo2121@gmail.com>
2020-03-14 23:46:29 -0600

    SQL modify

--- springboot-servicio-usuarios/src/main/resources/import.sql
+++ springboot-servicio-usuarios/src/main/resources/import.sql
@@ -1,2 +1,9 @@
-INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('diegoarturomg', '123456', 1, 'Arturo', 'Morales', 'diegoarturo@gmail.com')
-INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('omardavidmg', '123456', 1, 'Omar', 'Morales', 'omardavid@gmail.com')
+INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('diegoarturomg', '123456', 1, 'Arturo', 'Morales', 'diegoarturo@gmail.com');
+INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('omardavidmg', '123456', 1, 'Omar', 'Morales', 'omardavid@gmail.com');
+
+INSERT INTO `roles` (nombre) VALUES ('ROLE_USER');
+INSERT INTO `roles` (nombre) VALUES ('ROLE_ADMIN');
+
+INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (1,1);
+INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (2,2);
+INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (2,1);
