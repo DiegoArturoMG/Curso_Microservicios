e7090a2b2840892d6946898df4ad9f1601ef54d0
Diego Arturo MG <diegoarturo2121@gmail.com>
2020-03-24 01:11:44 -0600

    oauth

--- springboot-servicio-usuarios/src/main/resources/import.sql
+++ springboot-servicio-usuarios/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('diegoarturomg', '123456', 1, 'Arturo', 'Morales', 'diegoarturo@gmail.com');
-INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('omardavidmg', '123456', 1, 'Omar', 'Morales', 'omardavid@gmail.com');
+INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('diegoarturomg', '$2a$10$kGPpDqpR5WVP.QL/ttpxAevkhBfFm1XQgq3P.Sgt18j8UPqeScLvm', 1, 'Arturo', 'Morales', 'diegoarturo@gmail.com');
+INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('omardavidmg', '$2a$10$JAHskDJ7UJlhECRU3nsOFOmKHIZw3lCvllN2KS87Im/A3GzhtLHPe', 1, 'Omar', 'Morales', 'omardavid@gmail.com');
 
 INSERT INTO `roles` (nombre) VALUES ('ROLE_USER');
 INSERT INTO `roles` (nombre) VALUES ('ROLE_ADMIN');
